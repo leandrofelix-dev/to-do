f0c1fc8f7479d934aec15e8714b578730df66c18
lehinfo.felix <lehinfo.felix@gmail.com>
2021-10-23 14:03:17 -0300

    implementação da task no HTML

--- src/js/main.js
+++ src/js/main.js
@@ -9,7 +9,7 @@ function setRandomColor() {
         'purple',
         'pink'
     ];
-    let min = 0;
+    const min = 0;
     let max = (colors.length) - 1;
     let selectedColor = Math.floor(Math.random() * (max - min + 1)) + min;
     let defaultColorPreview = colors[selectedColor];
@@ -22,21 +22,6 @@ function setRandomColor() {
     add_task_button.style.background = defaultColor;
 }
 
-function adicionarTarefa() {
-    const inputNovo = document.createElement('input');
-    const labelNovo = document.createElement('label');
-    const elementoAtual = document.getElementById("e");
-    const box = document.querySelector('#card-form');
-    let conteudo = document.createTextNode("Batatinha frita 123");
-
-    inputNovo.type = 'checkbox';
-    labelNovo.appendChild(conteudo);
-
-    box.insertBefore(labelNovo, elementoAtual);
-    box.insertBefore(inputNovo, labelNovo);
-}
-
-
 function closeTaskManager() {
     const task_manager = document.querySelector('#input-task-out')
     task_manager.style.display = 'none';
@@ -45,4 +30,37 @@ function closeTaskManager() {
 function openTaskManager() {
     const task_manager = document.querySelector('#input-task-out');
     task_manager.style.display = 'flex';
+}
+
+function addTask() {
+    const add_task_input = document.querySelector('#add-task-input');
+    let new_task = add_task_input.value;
+    document.querySelector('#add-task-input').value = '';
+
+    if (new_task == '') {
+        alert('insira um valor no campo')
+    } else {
+
+        // DAQUI PRA BAIXO PODER DAR MERDA
+
+        const inputNovo = document.createElement('input');
+        const labelNovo = document.createElement('label');
+        const elementoAtual = document.getElementById("e");
+        const box = document.querySelector('#card-form');
+        let conteudo = document.createTextNode(new_task);
+
+        inputNovo.type = 'checkbox';
+        labelNovo.appendChild(conteudo);
+
+        box.insertBefore(labelNovo, elementoAtual);
+        box.insertBefore(inputNovo, labelNovo);
+
+        let taskLenght = document.querySelectorAll('label').length;
+        const taskLenghtIndicator = document.querySelector('#task-lenght');
+        taskLenghtIndicator.innerText = taskLenght;
+        document.querySelector('#input-task-out').style.display = 'none';
+
+    }
+
+
 }
