21cbe346ff841a020598e32e0376faf951d5a08d
lehinfo.felix <lehinfo.felix@gmail.com>
2021-10-23 17:45:10 -0300

    Implementação do HTML da tela de escolha de cor

--- src/js/main.js
+++ src/js/main.js
@@ -17,14 +17,21 @@ function setRandomColor() {
 
     let color_tag = document.querySelector('#color-tag');
     let add_task_button = document.querySelector('#add-task-button');
+    let task_manager_button = document.querySelector('#task-manager-button');
+    let add_task_input = document.querySelector('#add-task-input');
 
     color_tag.style.background = defaultColor;
     add_task_button.style.background = defaultColor;
+    task_manager_button.style.background = defaultColor;
+    add_task_input.style.borderColor = defaultColor;
 }
 
-function closeTaskManager() {
-    const task_manager = document.querySelector('#input-task-out')
-    task_manager.style.display = 'none';
+function openSelectColor() {
+    document.querySelector('#select-color').style.display = 'block';
+}
+
+function closeSelectColor() {
+    document.querySelector('#select-color').style.display = 'none';
 }
 
 function openTaskManager() {
@@ -38,10 +45,8 @@ function addTask() {
     document.querySelector('#add-task-input').value = '';
 
     if (new_task == '') {
-        alert('insira um valor no campo')
+        document.querySelector('#alert-span').style.display = 'block';
     } else {
-
-        // DAQUI PRA BAIXO PODER DAR MERDA
 
         const inputNovo = document.createElement('input');
         const labelNovo = document.createElement('label');
@@ -59,6 +64,8 @@ function addTask() {
         const taskLenghtIndicator = document.querySelector('#task-lenght');
         taskLenghtIndicator.innerText = taskLenght;
         document.querySelector('#input-task-out').style.display = 'none';
+        document.querySelector('#alert-span').style.display = 'none';
+        document.querySelector('#tasks').style.padding = '10px';
 
     }
 
